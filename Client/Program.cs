d779a8ae963e5a4c4735c68fb93c441d1b0b58fe
AliShamil <elisamilzade@gmail.com>
2023-03-13 17:59:29 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using CommandLibrary;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+string ipAddress = "127.0.0.1";
+int port = 12345;
+TcpClient client = new TcpClient(ipAddress, port);
+NetworkStream stream = client.GetStream();
+while (true)
+{
+    Console.Write("Enter:");
+    var text = Console.ReadLine();
+
+    Command myObject = new Command();
+    myObject.Text = text;
+    myObject.Param = text;
+    BinaryFormatter formatter = new BinaryFormatter();
+    byte[] buffer = new byte[1024];
+    using (var ms = new MemoryStream())
+    {
+        formatter.Serialize(ms, myObject);
+        buffer = ms.ToArray();
+    }
+    stream.Write(buffer, 0, buffer.Length);
+    stream.Flush();
+
+    if (text == "1")
+    {
+        byte[] receiveBuffer = new byte[1024];
+        int receivedBytes = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+        string receivedText = Encoding.ASCII.GetString(receiveBuffer, 0, receivedBytes);
+        Console.WriteLine("Received: " + receivedText);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using CommandLibrary;
+
+int port = 12345;
+TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+listener.Start();
+
+TcpClient remoteClient = listener.AcceptTcpClient();
+NetworkStream remoteStream = remoteClient.GetStream();
+while (true)
+{
+    BinaryFormatter formatter = new BinaryFormatter();
+    byte[] receivedBuffer = new byte[1024];
+    int receivedBytes = remoteStream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+
+    using (var ms = new MemoryStream(receivedBuffer))
+    {
+        Command receivedObject = (Command)formatter.Deserialize(ms);
+        Console.WriteLine("Received command: Text={0}, Param={1}", receivedObject.Text, receivedObject.Param);
+
+        if (receivedObject.Text == "1")
+        {
+            byte[] sendBuffer = Encoding.ASCII.GetBytes("Salam Aleykum");
+            remoteStream.Write(sendBuffer, 0, sendBuffer.Length);
+            remoteStream.Flush();
+        }
+    }
+}
