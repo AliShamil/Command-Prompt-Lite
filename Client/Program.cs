3c11f684938fae29bea484e28555d5a48c07a0e3
AliShamil <elisamilzade@gmail.com>
2023-03-14 18:51:23 +0400

    Change serialization/deserialization to json

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,38 @@
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using Newtonsoft.Json;
+//string ipAddress = "127.0.0.1";
+//int port = 12345;
+//TcpClient client = new TcpClient(ipAddress, port);
+//NetworkStream stream = client.GetStream();
+//while (true)
+//{
+//    Console.Write("Enter:");
+//    var text = Console.ReadLine();
+
+//    Command myObject = new Command();
+//    myObject.Text = text;
+//    myObject.Param = text;
+//    BinaryFormatter formatter = new BinaryFormatter();
+//    byte[] buffer = new byte[1024];
+//    using (var ms = new MemoryStream())
+//    {
+//        formatter.Serialize(ms, myObject);
+//        buffer = ms.ToArray();
+//    }
+//    stream.Write(buffer, 0, buffer.Length);
+//    stream.Flush();
+
+//    if (text == "1")
+//    {
+//        byte[] receiveBuffer = new byte[1024];
+//        int receivedBytes = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+//        string receivedText = Encoding.ASCII.GetString(receiveBuffer, 0, receivedBytes);
+//        Console.WriteLine("Received: " + receivedText);
+//    }
+//}
+
 
 string ipAddress = "127.0.0.1";
 int port = 12345;
@@ -9,19 +41,18 @@ TcpClient client = new TcpClient(ipAddress, port);
 NetworkStream stream = client.GetStream();
 while (true)
 {
-    Console.Write("Enter:");
+    Console.Write($"{Environment.CurrentDirectory}");
     var text = Console.ReadLine();
-
+    
+    //var t = text.Split(' ');
+    
     Command myObject = new Command();
-    myObject.Text = text;
-    myObject.Param = text;
-    BinaryFormatter formatter = new BinaryFormatter();
-    byte[] buffer = new byte[1024];
-    using (var ms = new MemoryStream())
-    {
-        formatter.Serialize(ms, myObject);
-        buffer = ms.ToArray();
-    }
+    myObject.Text = text.Trim();
+    myObject.Param = text.Trim();
+
+    string serializedObject = JsonConvert.SerializeObject(myObject);
+    byte[] buffer = Encoding.UTF8.GetBytes(serializedObject);
+
     stream.Write(buffer, 0, buffer.Length);
     stream.Flush();
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,34 @@ using System.Net;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using CommandLibrary;
+using Newtonsoft.Json;
+
+//int port = 12345;
+//TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+//listener.Start();
+
+//TcpClient remoteClient = listener.AcceptTcpClient();
+//NetworkStream remoteStream = remoteClient.GetStream();
+//while (true)
+//{
+//    BinaryFormatter formatter = new BinaryFormatter();
+//    byte[] receivedBuffer = new byte[1024];
+//    int receivedBytes = remoteStream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+
+//    using (var ms = new MemoryStream(receivedBuffer))
+//    {
+//        Command receivedObject = (Command)formatter.Deserialize(ms);
+//        Console.WriteLine("Received command: Text={0}, Param={1}", receivedObject.Text, receivedObject.Param);
+
+//        if (receivedObject.Text == "1")
+//        {
+//            byte[] sendBuffer = Encoding.ASCII.GetBytes("Salam Aleykum");
+//            remoteStream.Write(sendBuffer, 0, sendBuffer.Length);
+//            remoteStream.Flush();
+//        }
+//    }
+//}
 
 int port = 12345;
 TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
@@ -12,21 +40,19 @@ TcpClient remoteClient = listener.AcceptTcpClient();
 NetworkStream remoteStream = remoteClient.GetStream();
 while (true)
 {
-    BinaryFormatter formatter = new BinaryFormatter();
     byte[] receivedBuffer = new byte[1024];
     int receivedBytes = remoteStream.Read(receivedBuffer, 0, receivedBuffer.Length);
 
+    string serializedObject = Encoding.UTF8.GetString(receivedBuffer, 0, receivedBytes);
+    Command receivedObject = JsonConvert.DeserializeObject<Command>(serializedObject);
 
-    using (var ms = new MemoryStream(receivedBuffer))
+    Console.WriteLine("Received command: Text={0}, Param={1}", receivedObject.Text, receivedObject.Param);
+
+    if (receivedObject.Text == "1")
     {
-        Command receivedObject = (Command)formatter.Deserialize(ms);
-        Console.WriteLine("Received command: Text={0}, Param={1}", receivedObject.Text, receivedObject.Param);
-
-        if (receivedObject.Text == "1")
-        {
-            byte[] sendBuffer = Encoding.ASCII.GetBytes("Salam Aleykum");
-            remoteStream.Write(sendBuffer, 0, sendBuffer.Length);
-            remoteStream.Flush();
-        }
+        string sendText = "Salam Aleykum";
+        byte[] sendBuffer = Encoding.UTF8.GetBytes(sendText);
+        remoteStream.Write(sendBuffer, 0, sendBuffer.Length);
+        remoteStream.Flush();
     }
 }
